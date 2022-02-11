bbcd48f8d55d40b1e2fb495b1c0f897de6871c5f
courage331 <courage331@gmail.com>
2022-02-11 20:03:30 +0900

    db connection 없이 실행되도록 추가

--- src/main/java/kr/co/firestock/FireStockApplication.java
+++ src/main/java/kr/co/firestock/FireStockApplication.java
@@ -3,13 +3,14 @@ package kr.co.firestock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
 @EntityScan(basePackages = {"kr.co.firestock.vo"})
 @EnableJpaRepositories(basePackages = {"kr.co.firestock.repository"})
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class FireStockApplication {
 
 	public static void main(String[] args) {
